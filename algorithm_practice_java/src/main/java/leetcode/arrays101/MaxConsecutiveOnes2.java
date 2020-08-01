7673cdf29860014a901ce563058b18e149538f17
s_jaewoo <yureka1112@gmail.com>
2020-08-01 21:36:23 +0900

    [ing..] mCaxConsecutive

---
+++ algorithm_practice_java/src/main/java/leetcode/arrays101/MaxConsecutiveOnes2.java
@@ -0,0 +1,10 @@
+package leetcode.arrays101;
+
+/**
+ * url : https://leetcode.com/explore/learn/card/fun-with-arrays/523/conclusion/3230/
+ */
+public class MaxConsecutiveOnes2 {
+    public static int findMaxConsecutiveOnes(int[] nums) {
+        return nums.length;
+    }
+}
---
+++ algorithm_practice_java/src/test/java/leetcode/arrays101/MaxConsecutiveOnes2Test.java
@@ -0,0 +1,22 @@
+package leetcode.arrays101;
+
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+public class MaxConsecutiveOnes2Test {
+
+    private static Stream<Arguments> provider() {
+        return Stream.of(
+
+        )
+
+    }
+
+    @ParameterizedTest
+    @MethodSource("provider")
+    public void findMaxConsecutiveOnes() {
+    }
+}
