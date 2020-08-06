b3afdff97060f2189d7efcc731bea199e03dee1a
s_jaewoo <yureka1112@gmail.com>
2020-08-06 23:34:41 +0900

    solve max consecutive ones 2

--- algorithm_practice_java/src/main/java/leetcode/arrays101/MaxConsecutiveOnes2.java
+++ algorithm_practice_java/src/main/java/leetcode/arrays101/MaxConsecutiveOnes2.java
@@ -5,6 +5,27 @@ package leetcode.arrays101;
  */
 public class MaxConsecutiveOnes2 {
     public static int findMaxConsecutiveOnes(int[] nums) {
-        return nums.length;
+        int max = -1;
+        int cur = 0;
+        int idx = 0;
+        Boolean first = true;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                cur++;
+            } else if (nums[i] == 0 && first) {
+                cur++;
+                idx = i;
+                first = false;
+            } else if (nums[i] == 0 && !first) {
+                if (max < cur) {
+                    max = cur;
+                }
+                cur = 0;
+                first = true;
+                i = idx;
+            }
+        }
+
+        return (max < cur) ? cur : max;
     }
 }
--- algorithm_practice_java/src/test/java/leetcode/arrays101/MaxConsecutiveOnes2Test.java
+++ algorithm_practice_java/src/test/java/leetcode/arrays101/MaxConsecutiveOnes2Test.java
@@ -6,17 +6,25 @@ import org.junit.jupiter.params.provider.MethodSource;
 
 import java.util.stream.Stream;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MaxConsecutiveOnes2Test {
 
     private static Stream<Arguments> provider() {
         return Stream.of(
-
-        )
+                Arguments.of(new int[]{1, 0, 1, 1, 0}, 4),
+                Arguments.of(new int[]{0, 0, 0, 0, 0}, 1),
+                Arguments.of(new int[]{1, 1, 1, 1, 1, 1}, 6),
+                Arguments.of(new int[]{1, 0, 1, 1, 1}, 5),
+                Arguments.of(new int[]{1, 0, 1, 1, 0, 1, 1, 1, 0, 0}, 6),
+                Arguments.of(new int[]{1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1}, 9)
+        );
 
     }
 
     @ParameterizedTest
     @MethodSource("provider")
-    public void findMaxConsecutiveOnes() {
+    public void findMaxConsecutiveOnes(int nums[], int expected) {
+        assertThat(MaxConsecutiveOnes2.findMaxConsecutiveOnes(nums)).isEqualTo(expected);
     }
 }
