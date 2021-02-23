9eb1562ee48b26984cd10909002929ec47c2fff8
dev-owner <yureka1112@gmail.com>
2021-02-24 05:44:24 +0900

    [SOLVE] symmetric_tree iteratively

--- algorithm_practice_python/leetcode/Graph/symmetric_tree.py
+++ algorithm_practice_python/leetcode/Graph/symmetric_tree.py
@@ -6,20 +6,41 @@ class TreeNode:
 
 
 class Solution:
+    # recursive
+    # def isSymmetric(self, root: TreeNode) -> bool:
+    #     """
+    #     :param root: btree root node
+    #     :return: is symmetric true false
+    #     """
+    #     return self.isMirror(root, root)
+    #
+    # def isMirror(self, left, right):
+    #     if not left and not right:
+    #         return True
+    #
+    #     if not left or not right:
+    #         return False
+    #
+    #     return left.val == right.val \
+    #         and self.isMirror(left.left, right.right) \
+    #         and self.isMirror(left.right, right.left)
+
+    # iterate
     def isSymmetric(self, root: TreeNode) -> bool:
-        """
-        :param root: btree root node
-        :return: is symmetric true false
-        """
-        return self.isMirror(root, root)
-
-    def isMirror(self, left, right):
-        if not left and not right:
+        if root is None:
             return True
+        stack = [(root.left, root.right)]
+        while stack:
+            cur = stack.pop()
+            left, right = cur[0], cur[1]
+            if not left and not right:
+                continue
+            if not left or not right:
+                return False
+            if left.val == right.val:
+                stack.append((left.left, right.right))
+                stack.append((left.right, right.left))
+            else:
+                return False
+        return True
 
-        if not left or not right:
-            return False
-
-        return left.val == right.val \
-            and self.isMirror(left.left, right.right) \
-            and self.isMirror(left.right, right.left)
