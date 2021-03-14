ce302a5f3a40cf8c82e3f4b3a3a3c4a4ab2b7c8d
dev-owner <yureka1112@gmail.com>
2021-03-14 23:40:14 +0900

    [SOLVE-kotlin] beset time to buy and sell stock with kadane's algorithm

--- algorithm_practice_java/src/main/java/leetcode/default/BestTimeBuySellStock.kt
+++ algorithm_practice_java/src/main/java/leetcode/default/BestTimeBuySellStock.kt
@@ -1,16 +1,29 @@
 package leetcode.default
 
-import kotlin.math.min
 import kotlin.math.max
 
 class Solution {
+    //    fun maxProfit(prices: IntArray): Int {
+//        var maxProfit = 0
+//        var minPrice = Int.MAX_VALUE
+//        for (price in prices) {
+//            minPrice = min(price, minPrice)
+//            maxProfit = max(price - minPrice, maxProfit)
+//        }
+//        return maxProfit
+//    }
+    //using kadane's algorithm
+    // maxcur += max(0, price[i]- price[i-1])
     fun maxProfit(prices: IntArray): Int {
-        var maxProfit = 0
-        var minPrice = Int.MAX_VALUE
-        for (price in prices) {
-            minPrice = min(price, minPrice)
-            maxProfit = max(price - minPrice, maxProfit)
+        var maxCur = 0
+        var maxSoFar = 0
+
+        (1 until prices.size).forEach {
+            maxCur += prices[it] - prices[it - 1]
+            maxCur = max(0, maxCur)
+            maxSoFar = max(maxCur, maxSoFar)
         }
-        return maxProfit
+
+        return maxSoFar
     }
 }
