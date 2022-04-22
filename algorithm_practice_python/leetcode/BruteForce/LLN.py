940b05bcca5a663af33227199eef6c0a5f184ed1
dev-owner <yureka1112@gmail.com>
2022-04-23 08:10:22 +0900

    [이코테] 큰수의법칙#2

--- algorithm_practice_python/leetcode/BruteForce/LLN.py
+++ algorithm_practice_python/leetcode/BruteForce/LLN.py
@@ -1,6 +1,7 @@
 class LLN:
+
     @staticmethod
-    def run(n=0, m=0, k=0, N=""):
+    def basic(n=0, m=0, k=0, N=""):
         # N, M, K 공백 구분 입력
         if not (n and m and k):
             n, m, k = map(int, input().split())
@@ -28,6 +29,31 @@ class LLN:
             m -= 1
         return result
 
+    @staticmethod
+    def advanced(n=0, m=0, k=0, N=""):
+        # 반복되는 수열
+        # N, M, K 공백 구분 입력
+        if not (n and m and k):
+            n, m, k = map(int, input().split())
+        # N개의 수를 공백으로 구분하여 입력
+        if not N:
+            data = list(map(int, input().split()))
+        else:
+            data = list(map(int, N.split()))
+
+        data.sort()
+        first = data[n - 1]
+        second = data[n - 2]
+
+        count = int(m / (k + 1)) * k
+        count += m % (k + 1)
+
+        result = 0
+        result += count * first
+        result += (m - count) * second
+        return result
+
 
 if __name__ == '__main__':
-    print(LLN.run())
+    print(LLN.basic())
+    print(LLN.advanced())
--- algorithm_practice_python/leetcode/BruteForce/tests/test_LLN.py
+++ algorithm_practice_python/leetcode/BruteForce/tests/test_LLN.py
@@ -3,8 +3,13 @@ from unittest import TestCase
 from leetcode.BruteForce.LLN import LLN
 
 
-class TestBinarySearch(TestCase):
-    def test_search(self):
+class TestLLN(TestCase):
+    def test_basic(self):
         n, m, k = 5, 8, 3
         N = "2 4 5 4 6"
-        self.assertEqual(46, LLN.run(n, m, k, N))
+        self.assertEqual(46, LLN.basic(n, m, k, N))
+
+    def test_advanced(self):
+        n, m, k = 5, 7, 2
+        N = "3 4 3 4 3"
+        self.assertEqual(28, LLN.advanced(n, m, k, N))
