88fef4b800c6ec47e05e97a9ecf741b83d2a457b
Jaewoo Sung <sjaewoo@amazon.com>
2022-09-19 23:17:19 +0900

    add array easy

--- algorithm_practice_python/leetcode/arrays101/ SearchInsertPosition.py
+++ algorithm_practice_python/leetcode/arrays101/ SearchInsertPosition.py
@@ -1,8 +1,10 @@
+import bisect
+import math
 from typing import List
 
 
 class Solution:
-    def searchInsert(self, nums: List[int], target: int) -> int:
+    def searchInsert_old(self, nums: List[int], target: int) -> int:
         if target in nums:
             return nums.index(target)
 
@@ -11,6 +13,22 @@ class Solution:
                 return i
 
         return len(nums)
+
+    def searchInsert_bi(self, nums: List[int], target: int) -> int:
+        left, right = 0, len(nums) - 1
+        while left <= right:
+            mid = (left + right) // 2
+            if nums[mid] == target:
+                return mid
+            if nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+        return left
+
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        return bisect.bisect_left(nums,target)
+
 
 
 if __name__ == '__main__':
