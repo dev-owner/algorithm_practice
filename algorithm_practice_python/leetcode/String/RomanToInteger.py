43a7e441ac629ab5c5d6619247c901a47b55ae36
Jaewoo Sung <sjaewoo@amazon.com>
2022-09-20 23:26:46 +0900

    add string easy

---
+++ algorithm_practice_python/leetcode/String/RomanToInteger.py
@@ -0,0 +1,25 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman_map = {
+            "I": 1,
+            "V": 5,
+            "X": 10,
+            "L": 50,
+            "C": 100,
+            "D": 500,
+            "M": 1000
+        }
+
+        s = s.replace("IV", "IIII").replace("IX", "VIIII")
+        s = s.replace("XL", "XXXX").replace("XC", "LXXXX")
+        s = s.replace("CD", "CCCC").replace("CM", "DCCCC")
+
+        result = 0
+        for c in s:
+            result += roman_map[c]
+        return result
+
+
+if __name__ == '__main__':
+    s = "MCMXCIV"
+    print(Solution().romanToInt(s), 1994)
