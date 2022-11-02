d8e0d5e712602b7cd373a8922f35d7d7deca38b0
dev-owner <yureka1112@gmail.com>
2022-11-02 09:06:41 +0900

    fix 300

--- algorithm_practice_python/leetcode/DP/300.py
+++ algorithm_practice_python/leetcode/DP/300.py
@@ -4,21 +4,19 @@ from typing import List
 class Solution:
     def lengthOfLIS(self, nums: List[int]) -> int:
         # return the length of the longest strictly increasing subsequence.
-        # https://leetcode.com/problems/longest-increasing-subsequence/solutions/1326552/optimization-from-brute-force-to-dynamic-programming-explained/?orderBy=most_votes
-        result = 0
-        prev = 0
-        cur_sum = 1
-        for current in nums:
-            if prev > current:
-                result = cur_sum if cur_sum > result else result
-                cur_sum = 1
-            cur_sum += 1
-            prev = current
-        return result
-
+        # 최장 증가 부분 수열
+        # 주어진 수열에서 오름차순으로 정렬된 가장 긴 부분 수열을 찾는 문제, 유일할 필요는 없다
+        # 현재값이 이전값보다 작거나같으면 고르면 안되고 크면 넘어가야 한다
+        size = len(nums)
+        dp = [1] * size
+        for i in range(size):
+            for j in range(i):
+                if nums[j] < nums[i] and dp[i] < dp[j] + 1:
+                    dp[i] = dp[j] + 1
+        return max(dp)
 
 
 if __name__ == '__main__':
-    nums = [10, 9, 2, 5, 3, 7, 101, 18]
+    nums = [0, 1, 0, 3, 2, 3]
     ans = Solution().lengthOfLIS(nums)
     print(f'{ans}, 4')
