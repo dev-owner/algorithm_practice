7fa9caada4f7b95344ef0b26dcc79115ac5c7994
dev-owner <yureka1112@gmail.com>
2022-11-12 06:27:06 +0900

    add 118

--- algorithm_practice_python/leetcode/DP/118.py
+++ algorithm_practice_python/leetcode/DP/118.py
@@ -10,21 +10,18 @@ class Solution:
             1 2 1
             1 3 3 1
             1 4 6 4 1
+
+            time : n^2
+            space : n^2
         """
-
-        ans = []
-
-        for i in range(numRows):
-            for j in range(i):
-                if j == 0 or j == i:
-                    ans = ans + [[1]]
-                else:
-                    pass
-
-
+        ans = [[1] * i for i in range(1, numRows + 1)]
+        for i in range(1, numRows):
+            for j in range(1, i):
+                ans[i][j] = ans[i - 1][j] + ans[i - 1][j - 1]
         return ans
+
+
 if __name__ == '__main__':
     numRows = 5
     ans = Solution().generate(numRows)
     print(f'{ans}, [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]')
-
