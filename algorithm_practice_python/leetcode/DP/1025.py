8c79c64f4c9a43a526bbf5087d4870206d8e5a1a
dev-owner <yureka1112@gmail.com>
2022-11-14 08:43:41 +0900

    add 1025

--- algorithm_practice_python/leetcode/DP/1025.py
+++ algorithm_practice_python/leetcode/DP/1025.py
@@ -1,20 +1,23 @@
-
-def get_max_divisor(n):
-    ans = 1
-    for i in range(1, n):
-        if n % i == 0:
-            ans = i
-    return ans
-
 class Solution:
     def divisorGame(self, n: int) -> bool:
-        i = 1
-        while True:
-            if n <= 1 : break
-            x = get_max_divisor(n)
-            n = n - x
-            i = i + 1
-        return True if i % 2 == 0 else False
+        """
+            alice가 게임 먼저 시작
+            약수로 현재 숫자 빼면서 더이상 뺄 수 없으면 지는게임
+            optimal하게 움직인다고 가정 -> 특정 숫자에서 결과는 정해짐
+            dp[i] = True if the player starting with number i wins, else False
+            1 <= i <= 1000
+            n = 2 -> true
+            n = 3 -> false
+            n = 4 -> true
+        """
+
+        dp = [False for _ in range(n+1)]
+        for i in range(n+1):
+            for j in range(1, i//2+1):
+                if i % j == 0 and (not dp[i-j]):
+                    dp[i] = True
+        return dp[n]
+
 
 
 if __name__ == '__main__':
