b5b31b64d21071a4103afe966a60883bb1ecddbd
dev-owner <yureka1112@gmail.com>
2022-11-17 08:49:31 +0900

    done 1971

--- algorithm_practice_python/leetcode/Graph/1971.py
+++ algorithm_practice_python/leetcode/Graph/1971.py
@@ -1,19 +1,13 @@
-from collections import defaultdict
+from collections import defaultdict, deque
 from typing import List
 
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        """
-            dfs
-        """
-
         neighbors = defaultdict(list)
         for n1, n2 in edges:
             neighbors[n1].append(n2)
             neighbors[n2].append(n1)
-
-        print(edges)
 
         def dfs(node, end, seen):
             if node == end:
@@ -30,6 +24,23 @@ class Solution:
         return dfs(source, destination, seen)
 
 
+    def validPath2(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        neighbors = defaultdict(list)
+        for n1, n2 in edges:
+            neighbors[n1].append(n2)
+            neighbors[n2].append(n1)
+
+        visitied = [False] * n
+        q = [source]
+        while q:
+            curr = q.pop(0)
+            if curr == destination: return True
+            elif curr in neighbors and not visitied[curr]:
+                q.extend(neighbors[curr])
+            visitied[curr] = True
+        return False
+
+
 
 
 if __name__ == '__main__':
@@ -37,5 +48,5 @@ if __name__ == '__main__':
     edges = [[0,1],[1,2],[2,0]]
     source = 0
     destination = 2
-    ans = Solution().validPath(n, edges, source, destination)
+    ans = Solution().validPath2(n, edges, source, destination)
     print(f'{ans}, true')
