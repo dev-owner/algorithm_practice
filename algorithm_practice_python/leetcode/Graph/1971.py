e5f51689b7190c6cfc66b01e201d2a6515df76ff
dev-owner <yureka1112@gmail.com>
2022-11-19 09:10:06 +0900

    refactor 1971 dfs

--- algorithm_practice_python/leetcode/Graph/1971.py
+++ algorithm_practice_python/leetcode/Graph/1971.py
@@ -1,52 +1,36 @@
-from collections import defaultdict, deque
+from collections import defaultdict
 from typing import List
 
 
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+        # dfs
+        # depth first search
+        # 방문 표시, 계속 파고듬
+        # 인접 리스트 준비 필요
         neighbors = defaultdict(list)
-        for n1, n2 in edges:
-            neighbors[n1].append(n2)
-            neighbors[n2].append(n1)
+        for k, v in edges:
+            neighbors[k].append(v)
+            neighbors[v].append(k)
 
-        def dfs(node, end, seen):
-            if node == end:
-                return True
-            if node in seen:
-                return False
-            seen.add(node)
-            for n in neighbors[node]:
-                if dfs(n, end, seen):
+        visited = set()
+
+        def dfs(node, end):
+            if node == end: return True
+            if node in visited: return False
+            visited.add(node)
+            for neighbor in neighbors[node]:
+                if dfs(neighbor, end):
                     return True
             return False
 
-        seen = set()
-        return dfs(source, destination, seen)
-
-
-    def validPath2(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        neighbors = defaultdict(list)
-        for n1, n2 in edges:
-            neighbors[n1].append(n2)
-            neighbors[n2].append(n1)
-
-        visitied = [False] * n
-        q = [source]
-        while q:
-            curr = q.pop(0)
-            if curr == destination: return True
-            elif curr in neighbors and not visitied[curr]:
-                q.extend(neighbors[curr])
-            visitied[curr] = True
-        return False
-
-
+        return dfs(source, destination)
 
 
 if __name__ == '__main__':
     n = 3
-    edges = [[0,1],[1,2],[2,0]]
+    edges = [[0, 1], [1, 2], [2, 0]]
     source = 0
     destination = 2
-    ans = Solution().validPath2(n, edges, source, destination)
+    ans = Solution().validPath(n, edges, source, destination)
     print(f'{ans}, true')
