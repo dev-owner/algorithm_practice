065ed62f162c6f085836b0befa9981e459a5e995
dev-owner <yureka1112@gmail.com>
2022-11-30 19:13:47 +0900

    add 617

--- algorithm_practice_python/leetcode/Graph/617.py
+++ algorithm_practice_python/leetcode/Graph/617.py
@@ -9,18 +9,36 @@ class TreeNode:
         self.right = right
 class Solution:
     def mergeTrees(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> Optional[TreeNode]:
-        if not root1 and not root2: return None
 
-        # if same level, add
-        # return new tree
+        if root1 and root2:
+            ans = TreeNode(root1.val + root2.val)
+            ans.left = self.mergeTrees(root1.left, root2.left)
+            ans.right = self.mergeTrees(root1.right, root2.right)
+            return ans
+        else:
+            return root1 or root2
 
-        if root1.val and root2.val:
-            root3.val = root1.val + root2.val
 
 
 
 if __name__ == '__main__':
 
 
-    ans = Solution().mergeTrees()
+    r14 = TreeNode(5)
+    r13= TreeNode(2)
+    r12 = TreeNode(3, r14)
+    r11 = TreeNode(1, r12, r13)
+
+    r24 = None
+    r25 = TreeNode(4)
+    r22 = TreeNode(1, r24, r25)
+    r26 = None
+    r27 = TreeNode(7)
+    r23 = TreeNode(3, r26, r27)
+    r21 = TreeNode(2, r22, r23)
+
+
+
+
+    ans = Solution().mergeTrees(r11, r21)
     print(f'{ans}, ')
