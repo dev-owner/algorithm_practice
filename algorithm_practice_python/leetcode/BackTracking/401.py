d86e56b08bea436b1d420159c7fc6a46cc12a244
dev-owner <yureka1112@gmail.com>
2022-12-21 08:21:18 +0900

    add 401

--- algorithm_practice_python/leetcode/BackTracking/401.py
+++ algorithm_practice_python/leetcode/BackTracking/401.py
@@ -8,13 +8,13 @@ class Solution:
             H 2^3 2^2 2^1 2^0
             M 2^5 2^4 2^3 2^2 2^1 2^0
             0 <= turnedOn <= 10
+            Given an integer turnedOn which represents the number of LEDs that are currently on (ignoring the PM), return all possible times the watch could represent. You may return the answer in any order.
         """
 
         hh = [8, 4, 2, 1]
         mm = [32, 16, 8, 4, 2, 1]
 
         # combination of hh:mm in turnedOn numbers
-        # 2면.. hh에서만 2개씩 모두 mm에서만 2개씩 모두 1개 hh 1개 mm 조합
         # hh는 최대 11까지
         # mm은 최대 60까지
         def get_hh(turnedOn: int):
@@ -49,9 +49,14 @@ class Solution:
 
         return get_hh_mm(turnedOn)
 
+    def readBinaryWatch2(self, turnedOn):
+        return ['%d:%02d' % (h, m)
+                for h in range(12) for m in range(60)
+                if (bin(h) + bin(m)).count('1') == turnedOn]
+
 
 if __name__ == '__main__':
     turnedOn = 3
-    ans = Solution().readBinaryWatch(turnedOn)
+    ans = Solution().readBinaryWatch2(turnedOn)
     print(len(ans))
     print(f'{ans}, ["0:01","0:02","0:04","0:08","0:16","0:32","1:00","2:00","4:00","8:00"]')
