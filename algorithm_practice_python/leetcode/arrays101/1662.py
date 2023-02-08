5e4254e26552408e887ab66c57e4764d47087fc4
dev-owner <yureka1112@gmail.com>
2023-02-08 23:26:32 +0900

    solve 1662

--- algorithm_practice_python/leetcode/arrays101/1662.py
+++ algorithm_practice_python/leetcode/arrays101/1662.py
@@ -2,10 +2,25 @@ from typing import List
 
 
 class Solution:
+    # time: O(N)
+    # space: O(N)
     def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
         return ''.join(word1) == ''.join(word2)
+
+    def sol2(self, w1, w2):
+        for c1, c2 in zip(self.generate(word1), self.generate(word2)):
+            if c1 != c2:
+                return False
+        return True
+
+    def generate(self, w):
+        for word in w:
+            for char in word:
+                yield char
+        yield None
+
 if __name__ == '__main__':
     word1 = ["ab", "c"]
     word2 = ["a", "bc"]
-    ans = Solution().arrayStringsAreEqual(word1, word2)
+    ans = Solution().sol2(word1, word2)
     print(f'{ans}, true')
